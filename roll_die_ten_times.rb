ee3e69ed71e3b21be4865ecf051688ebc9d804fb
Kara Capozzi <karacapozzi@Karas-MacBook.local>
2017-08-13 16:59:44 -0400

    add roll die ten times exercise 2

---
+++ roll_die_ten_times.rb
@@ -0,0 +1,31 @@
+
+
+def roll_die(roll)
+  roll = rand(6) + 1
+end
+
+
+puts "Roll the die."
+roll = gets.to_i
+
+
+10.times do
+p roll_die(roll)
+
+end
+
+
+
+
+
+#
+#
+#
+# def roll_die(roll)
+#   roll = rand(6) + 1
+# end
+# 10.times do |roll|
+# end
+#
+#   puts "Roll the die. Type: roll"
+#   roll = gets.to_s
